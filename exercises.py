57d1a3c53c840e449addb5e8f9df362f3714088b
sulla <sulladenis@mail.com>
2019-06-05 09:46:17 +1000

    add comment

--- exercises.py
+++ exercises.py
@@ -4,14 +4,14 @@ import random
 for n in range(10):
     tab = {} # Cловарь для таблици сложения -> dict('term1 + term2' : int(ответ))
     term1 = random.randint(0, 10) # Первое слогаемое -> случайное целое число
-    
-    # формирование словаря с таблицей ключей и значений 
-    for term2 in range(11): 
+
+    # формирование словаря с таблицей ключей и значений
+    for term2 in range(11):
         sample = str(term1) + ' + ' + str(term2) # ключь словаря -> str(пример)
-        tab[sample] = term1 + term2 # Добавление значений(решений)к ключам(примерам)
-        
+        tab[sample] = term1 + term2 # Добавление значений(решений) к ключам(примерам)
+
     # Выбор случайного примера из словаря
-    example = random.choice(list(tab.keys())) # random.choice() - случайный элемент
+    example = random.choice(tuple(tab.keys())) # random.choice() - случайный элемент
                                               # непустой последовательности
     print(example) # Печать примера
     answer = int(input('Ответ: ')) # Спрашиваем ответ пользователя
